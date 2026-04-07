8072da13a71147acf4e582676b89c1a9789ea014
JonasDall <jonaskd@live.no>
2026-04-07 14:20:04 +0200

    feat: source fix

--- vivado_project.tcl
+++ vivado_project.tcl
@@ -18,9 +18,9 @@
 proc checkRequiredFiles { origin_dir} {
   set status true
   set files [list \
- "[file normalize "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_tx.sv"]"\
- "[file normalize "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/timer.sv"]"\
- "[file normalize "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_rx.sv"]"\
+ "[file normalize "$origin_dir/src/uart_tx.sv"]"\
+ "[file normalize "$origin_dir/src/timer.sv"]"\
+ "[file normalize "$origin_dir/src/uart_rx.sv"]"\
   ]
   foreach ifile $files {
     if { ![file isfile $ifile] } {
@@ -157,24 +157,24 @@ if {[string equal [get_filesets -quiet sources_1] ""]} {
 # Set 'sources_1' fileset object
 set obj [get_filesets sources_1]
 set files [list \
- [file normalize "${origin_dir}/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_tx.sv"] \
- [file normalize "${origin_dir}/vivado_project/vivado_project.srcs/sources_1/imports/src/timer.sv"] \
- [file normalize "${origin_dir}/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_rx.sv"] \
+ [file normalize "${origin_dir}/src/uart_tx.sv"] \
+ [file normalize "${origin_dir}/src/timer.sv"] \
+ [file normalize "${origin_dir}/src/uart_rx.sv"] \
 ]
 add_files -norecurse -fileset $obj $files
 
 # Set 'sources_1' fileset file properties for remote files
-set file "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_tx.sv"
+set file "$origin_dir/src/uart_tx.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
-set file "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/timer.sv"
+set file "$origin_dir/src/timer.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
-set file "$origin_dir/vivado_project/vivado_project.srcs/sources_1/imports/src/uart_rx.sv"
+set file "$origin_dir/src/uart_rx.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
