b17da23be35603e4a8d55333ac3585d731d781cd
Eirik Holm <eirik@holm1.no>
2026-04-16 15:33:31 +0200

    Added WTA testbench and verified functionality

--- src/SNN_core/WTA.sv
+++ src/SNN_core/WTA.sv
@@ -3,21 +3,20 @@ module WTA #(
 ) (
     input  logic [N_OUTPUTS-1:0]            spk,
     input  logic [N_OUTPUTS-1:0][15:0]      pre_reset_mem,
-    output logic [$clog2(N_OUTPUTS)-1:0]    winner_idx
+    output logic [$clog2(N_OUTPUTS)-1:0]    winner_idx,
+    output logic                            winner_valid
 );
 
-  logic [N_OUTPUTS-1:0] pool;
-  logic [15:0]          max_mem;
+  logic [15:0] max_mem;
 
-  // Spiking neurons compete; if none spiked, all compete
-  assign pool = |spk ? spk : '1;
+  // A winner exists only when at least one neuron spiked
+  assign winner_valid = |spk;
 
   always_comb begin
-    // Initialise to first pool member so winner_idx is always in pool
     winner_idx = '0;
     max_mem    = '0;
     for (int i = 0; i < N_OUTPUTS; i++) begin
-      if (pool[i] && pre_reset_mem[i] >= max_mem) begin
+      if (spk[i] && pre_reset_mem[i] >= max_mem) begin
         max_mem    = pre_reset_mem[i];
         winner_idx = i[$clog2(N_OUTPUTS)-1:0];
       end
