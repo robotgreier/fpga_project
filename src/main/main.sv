a951f6bc5b720551291959017dd32bfc1e67d061
JonasDall <jonaskd@live.no>
2026-04-20 13:28:43 +0200

    feat: added master_enable signal

--- src/main/main.sv
+++ src/main/main.sv
@@ -48,7 +48,7 @@ always @(posedge clk) begin
   end
 end
 
-wire  master_read, master_transmit; // Master signals
+wire  master_read, master_transmit, master_enable; // Master signals
 wire  [7:0] master_data;
 
 wire  verifier_ready, // Verifier signals
