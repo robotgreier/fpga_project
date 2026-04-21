ca5bb58847af1058632934bf58a5e00c8ec9b03c
EiHolm <eirik@holm1.no>
2026-04-21 20:13:28 +0200

    feat: completed module

--- src/main/weight_loader.sv
+++ src/main/weight_loader.sv
@@ -3,12 +3,22 @@ module weight_loader #(
     parameter int N_OUTPUTS    = 4,
     parameter int FEEDBACK     = 1,
   ) (
-    input logic clk, rst,
-    input logic data,
-    input logic adr,
+    input logic clk, rst, w_en,
+    input logic [7:0] data,
+    input logic [15:0] adr,
     output logic [7:0] w_next  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0]          
   );
 
+  logic [7:0] w_temp  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0];
 
+    always_ff @(posedge clk) begin : get_weight
+        // Decode adr into matrix indices
+        [j][ii]: j = adr / (N_INPUTS+FEEDBACK), ii = adr % (N_INPUTS+FEEDBACK);
+        if (w_en) begin
+            w_temp[j][ii] <= data;
+        end
+    end
+
+    assign w_next = w_temp;
 
 endmodule
