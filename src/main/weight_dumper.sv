6fe3f22dd8455ee41e0c195caeedb920cdd62a08
EiHolm <eirik@holm1.no>
2026-04-21 20:22:19 +0200

    feat: completed module

---
+++ src/main/weight_dumper.sv
@@ -0,0 +1,20 @@
+module weight_dumper #(
+    parameter int N_INPUTS     = 31,
+    parameter int N_OUTPUTS    = 4,
+    parameter int FEEDBACK     = 1
+  ) (
+    input logic clk, rst, en,
+    input logic [15:0] adr,
+    input logic [7:0] w_syn  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0],
+    output logic [((N_INPUTS+FEEDBACK)*N_OUTPUTS*8)-1:0] w_parallel_out
+  );
+
+    always_comb begin : parallelizer
+        for (int i = 0; i < N_OUTPUTS; i++) begin
+            for (int j = 0; j < (N_INPUTS + FEEDBACK); j++) begin
+                w_parallel_out[(i*COLS + j)*8 +: 8] = w_syn[i][j];
+            end
+        end
+    end
+
+endmodule
--- src/main/weight_loader.sv
+++ src/main/weight_loader.sv
@@ -6,19 +6,21 @@ module weight_loader #(
     input logic clk, rst, w_en,
     input logic [7:0] data,
     input logic [15:0] adr,
-    output logic [7:0] w_next  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0]          
+    output logic [7:0] w_next  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0]
   );
 
   logic [7:0] w_temp  [N_OUTPUTS-1:0][(N_INPUTS+FEEDBACK)-1:0];
 
-    always_ff @(posedge clk) begin : get_weight
-        // Decode adr into matrix indices
-        [j][ii]: j = adr / (N_INPUTS+FEEDBACK), ii = adr % (N_INPUTS+FEEDBACK);
-        if (w_en) begin
-            w_temp[j][ii] <= data;
-        end
+  always_ff @(posedge clk)
+  begin : get_weight
+    // Decode adr into matrix indices
+    [j][ii] : j = adr / (N_INPUTS+FEEDBACK), ii = adr % (N_INPUTS+FEEDBACK);
+    if (w_en)
+    begin
+      w_temp[j][ii] <= data;
     end
+  end
 
-    assign w_next = w_temp;
+  assign w_next = w_temp;
 
 endmodule
