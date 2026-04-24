09358d33bf0c5d00d23e416aef8bf052cf3adccb
Eirik Holm <eirik@holm1.no>
2026-04-24 13:55:55 +0200

    feat: added reward_en logic

--- src/main/dopamine_loader.sv
+++ src/main/dopamine_loader.sv
@@ -2,7 +2,8 @@ module dopamine_loader (
     input logic clk, rst,
     input logic [7:0] data,
     input logic [7:0] adr,
-    output logic signed [3:0] dopamine
+    output logic signed [3:0] dopamine,
+    output logic reward_en
   );
 
   localparam int DOP_ADR = 199;
@@ -12,6 +13,13 @@ module dopamine_loader (
       dopamine = data[3:0];
     else
       dopamine = '0;
+  end
+
+  always_comb begin : get_reward_en
+    if (adr == DOP_ADR)
+      reward_en = data != 0;
+    else
+      reward_en = 1'b0;
   end
 
 endmodule
